728d14b267746082f878d7aef01946dee5747ba6
David Hatton <dhatton@turnitin.com>
2017-05-29 17:49:48 +0100

    Create install script
    ## Done
    - Set up basic config for $MYREPOS variable
    - Establish gitignore
    - Create readme
    - Create file for holding directory paths to additional folders
    
    ## ToDo
    - allow use of any variable name

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo "Setting up tool with central repo directory...\n"
+
+# Discern whether there is already a $MYREPOS set.
+if [[ -z ${MYREPOS+x} ]]; then 
+    # Set it up
+    echo "no variable \"\$MYREPOS\" detected.\nPlease enter a path to the folder containing your git repositories:"
+    read repoPath
+    echo "export MYREPOS=$repoPath" >> ~/.bash_profile
+    source ~/.bash_profile
+
+    # check it
+    if [[ -n $MYREPOS ]]; then echo "directory variable setup successful"
+    else echo "there was a problem setting up variable \"\$MYREPOS\"."
+    fi
+else 
+    if [[ -d $MYREPOS ]]; then
+        echo "There is already an exported variable \"\$MYREPOS\", currently set to $MYREPOS.\nWould you like to use this variable as the main repository?"
+        OPTIONS="yes no"
+        select opt in $OPTIONS; do
+            if [[ "$opt" == "yes" ]]; then
+                echo "OK, let's do it."
+                break
+            elif [[ "$opt" == "no" ]]; then
+                echo "That variable name is required. Try renaming that variable to something else and starting again.\n"
+                exit
+            else echo "Bad entry, please select (1) for yes or (2) for no."
+            fi
+        done
+    else
+        echo "There is already an exported variable \"\$MYREPOS\", but it is not a directory. That variable is required for setup and config./nTry renaming that variable and starting again."
+    fi
+fi
+
+# Get other directories to use
+echo "Are there any other repositories that you wish to include for use by this tool?"
+select opt in $OPTIONS; do
+    if [[ "$opt" == "yes" ]]; then
+        # Write directories 
+        echo "Please enter any extra directories to include that are not in the parent directory covered by \$MYREPOS.\nEnter full paths, separated by spaces."
+        read extrasString
+        extras=($extrasString)
+        printf "%s\n" ${extras[@]} > ./lib/extras.txt
+        break
+    elif [[ "$opt" == "no" ]]; then
+        echo "" > ./lib/extras.txt
+        echo "Setup complete."
+        break
+    else echo "Bad entry, please select (1) for yes or (2) for no."
+    fi
+done
